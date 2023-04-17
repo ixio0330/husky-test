14ef936d1af77642be710a4d57caa2f5cc37883f
서나무 <104984874+ixio0330@users.noreply.github.com>
2023-04-17 15:29:30 +0900

    ✨ server app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const express = require('express');
 
+const app = express();
+
+app.use(express.static('wwwroot'));
+
+app.listen(3001, () => {
+  console.log('http://localhost:3001');
+});
+
+app.get('/', (_, res) => {
+  const filePath = path.join(__dirname, 'wwwroot', 'index.html');
+  res.sendFile(filePath);
+});
